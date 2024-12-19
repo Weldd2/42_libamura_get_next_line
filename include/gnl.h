6c5fc852d228559179b8561ea90d094375b40e2d
Your Name <you@example.com>
2024-12-19 04:51:31 +0100

    refactor gnl

--- include/gnl.h
+++ include/gnl.h
@@ -1,19 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   gnl_get_next_line.h                                :+:      :+:    :+:   */
+/*   gnl.h                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: antoinemura <antoinemura@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/02 17:28:48 by antoinemura       #+#    #+#             */
-/*   Updated: 2024/12/19 00:46:08 by antoinemura      ###   ########.fr       */
+/*   Updated: 2024/12/19 04:49:40 by antoinemura      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GNL_GET_NEXT_LINE_H
-# define GNL_GET_NEXT_LINE_H
+#ifndef GNL_H
+# define GNL_H
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 10
+#  define BUFFER_SIZE 1000000
 # endif
 # include <unistd.h>
 # include <stdlib.h>
--- src/gnl_get_next_line.c
+++ src/gnl_get_next_line.c
@@ -6,47 +6,48 @@
 /*   By: antoinemura <antoinemura@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/25 23:27:04 by antoinemura       #+#    #+#             */
-/*   Updated: 2024/12/19 00:43:56 by antoinemura      ###   ########.fr       */
+/*   Updated: 2024/12/19 04:50:54 by antoinemura      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "gnl_get_next_line.h"
+#include "gnl.h"
 
-bool	found_endline(char *buffer, int *size)
+bool	found_endline(char *line, int *pos)
 {
 	int	i;
 
 	i = 0;
-	while (buffer[i] != '\0' && buffer[i] != '\n')
-		i++;
-	if (buffer[i] == '\0')
+	if (line == NULL)
 		return (false);
-	return (*size = i, true);
+	while (line[i] != '\0' && line[i] != '\n')
+		i++;
+	if (line[i] == '\0')
+		return (false);
+	return (*pos = i, true);
 }
 
 char	*gnl_get_next_line(int fd)
 {
 	static char	buffer[BUFFER_SIZE + 1];
 	char		*line;
-	int			bytes_r;
-	int			size;
+	int			pos;
+	int			bytes_read;
+	size_t		line_size;
 
-	size = 0;
-	bytes_r = read(fd, buffer, BUFFER_SIZE);
-	while (found_endline(buffer, &size) == false)
-		bytes_r = read(fd, buffer, BUFFER_SIZE);
-	line = mem_mgc_alloc(sizeof(char) * size);
-	str_strlcpy(line, buffer, size);
+	line_size = BUFFER_SIZE;
+	line = mem_mgc_calloc(BUFFER_SIZE, sizeof(char));
+	str_strlcat(line, buffer, BUFFER_SIZE);
+	while (found_endline(line, &pos) == false)
+	{
+		bytes_read = read(fd, buffer, BUFFER_SIZE);
+		if (bytes_read == -1)
+			return (line);
+		line_size += BUFFER_SIZE;
+		line = mem_realloc(line, line_size - BUFFER_SIZE, line_size);
+		str_strlcat(line, buffer, line_size);
+	}
+	str_strlcpy(buffer, str_substr(line, pos + 1, str_strlen(line) - pos), \
+		BUFFER_SIZE);
+	line = str_substr(line, 0, pos + 1);
 	return (line);
-}
-
-#include <stdio.h>
-
-int	main(void)
-{
-	int fd = open("src/gnl_get_next_line.c", O_RDONLY);
-	if (fd == -1)
-		exit(EXIT_FAILURE);
-	printf("%s", gnl_get_next_line(fd));
-	return (0);
 }
